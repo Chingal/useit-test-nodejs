2a90bb292b45fd337762d1ffa617d8e36027d6cd
Rodrigo Chingal <rchingal@gmail.com>
2019-03-15 00:34:24 -0500

    Second commit

--- utils/Constants.js
+++ utils/Constants.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 //export const HOST = "http://13.59.74.41:";  // Amazon Ec2
-exports.HOST = "http://127.0.0.1:";
+exports.HOST = "http://3.85.113.134:";
 exports.PORT = 19321;
 //# sourceMappingURL=Constants.js.map
